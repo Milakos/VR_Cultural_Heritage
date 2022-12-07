b1381e98169804695fbd77a060094e6b08f30d41
Milakos <88663425+Milakos@users.noreply.github.com>
2022-12-08 00:06:55 +0200

    Post_Process_Package

--- Assets/Scripts/ButtonActionsController.cs
+++ Assets/Scripts/ButtonActionsController.cs
@@ -9,12 +9,13 @@ public class ButtonActionsController : MonoBehaviour
 {
     public InputActionProperty[] inputButtonAction;
     UnityEvent myEvent = new UnityEvent();
-    UnityEvent myEventTeleport = new UnityEvent();
-    int i;
+    public bool isPressedBool;
+    bool pressed;
+
     private void Start() 
     {
-        myEvent.AddListener(QuitActionButton);
-        myEventTeleport.AddListener(TeleportingToInteractSwitch);  
+        myEvent.AddListener(QuitActionButton);  
+                
     }
     // Update is called once per frame
     void Update()
@@ -23,6 +24,7 @@ public class ButtonActionsController : MonoBehaviour
     }
     public void ButtonControllersInput()
     {
+        
         foreach (var item in inputButtonAction)
         {
                 bool buttonAPressed = inputButtonAction[0].action.IsPressed();                
@@ -30,44 +32,27 @@ public class ButtonActionsController : MonoBehaviour
                 bool buttonXPressed = inputButtonAction[2].action.IsPressed();
                 bool buttonYPressed = inputButtonAction[3].action.IsPressed();
 
+                if (buttonAPressed == true)
+                    print("You Pressed " + inputButtonAction[0].action.name);
+                
                 if (buttonBPressed == true)
                 {
                     print("You Pressed " + inputButtonAction[1].action.name);
+                }  
+                if(buttonXPressed)
+                {
+                     print("You Pressed " + inputButtonAction[1].action.name);
                 }
-                if (buttonAPressed == true)
-                    print("You Pressed " + inputButtonAction[0].action.name);  
                 if (buttonYPressed)
                 {
                     print("You Pressed " + inputButtonAction[3].action.name);  
                     myEvent.Invoke(); 
-                }
-                if(buttonXPressed)
-                {
-                    //TO CHECK TOMORROW
-                    // if()
-                    // {
-                    //     print ("Button X Invoked");
-                    //     myEventTeleport.Invoke();
-                    // }
-                    // else
-                    // {        
-                    //     print ("Button X Invoked");
-                    //     myEventTeleport.Invoke();                         
-                    // }
-                } 
+                }  
         }
     }
 
     public void QuitActionButton()
     {
-        print("Invoke");
         FindObjectOfType<MainMenu>().QuitGame();
-    }
-
-    public void TeleportingToInteractSwitch()
-    {
-        print("Invoke Switch");
-        //Todo find a way to change a switch    
-        FindObjectOfType<TeleportSwitch>().SwitchLayerMask(i);
-    }
+    }   
 }
--- Assets/Scripts/TeleportSwitch.cs
+++ Assets/Scripts/TeleportSwitch.cs
@@ -6,40 +6,24 @@ public class TeleportSwitch : MonoBehaviour
 {
     [SerializeField] XRRayInteractor rayInteractor;
     [SerializeField] InteractionLayerMask[] mask;
-    
     // Start is called before the first frame update
     void Start()
     {
-        rayInteractor = GetComponent<XRRayInteractor>();
+        rayInteractor = GetComponent<XRRayInteractor>();    
     }
 
     // Update is called once per frame
-    public void SwitchLayerMask(int IndexLayer)
-    {
-        int index;
-        //nOT CHECK TO CHECKTO MOOROW
-        switch(IndexLayer)
-        {
-            case 0: 
-            if(IndexLayer == 0)
-            {
-                index = 0;
-                rayInteractor.interactionLayers = mask[index];
-            }
-            
-            break;
-            
-            case 1:
-            if(IndexLayer == 1) 
-            {
-                index = 1;
-                rayInteractor.interactionLayers = mask[index];
-            }
-            break;
-        }
-        print("InteractLayer");
-        // return IndexLayer;
-        // rayInteractor.interactionLayers = mask[IndexLayer];
-        
+    public void SwitchLayerMaskTeleport()
+    {      
+        int index = 0;
+        rayInteractor.interactionLayers = mask[index];
+        print("0");            
     }
+    public void SwitchLayerMaskMixed()
+    {
+        int index = 1;
+        rayInteractor.interactionLayers = mask[index];
+        print("1"); 
+    }
+
 }
